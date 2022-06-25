04dd404329c84ef8394aa54a14770e9086ca11ae
MSI\Victo <VictorCarmona9035@hotmail.com>
2022-06-25 13:12:42 +0200

    2-Creacion de la conexion a la BBDD

---
+++ Sistema.Datos/Conexion.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Datos
+{
+    public  class Conexion
+    {
+        //Almacena la base de datos a la que nos conectaremos
+        private string Base;
+        //Almacena el nombre del servidor al que nos conectaremos
+        private string Servidor;
+        //Almacenara el Usuario de la BBDD
+        private string Usuario;
+        //Almacenara la clave de la BBDD
+        private string Clave;
+        //Almacenara si estamos entrando con autenticacion de Windows o autenticacion de SQL Server
+        private bool Seguridad;
+
+        //Para la instancion de la conexion
+        private static Conexion Con = null;
+
+        //Constructor
+        private Conexion()
+        {
+            this.Base = "dbsistema";
+            this.Servidor = "MSI\\SQLEXPRESS";
+            this.Usuario = "sa";
+            this.Clave = "1234";
+            this.Seguridad = true;  //Autenticacion de Windows cuando esta a true
+        }
+
+        //Creamos la conexion
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection Cadena = new SqlConnection();
+
+            try
+            {
+                Cadena.ConnectionString = "Server=" + this.Servidor + "; Database= " + this.Base + ";";
+
+                //Validacion para ver que tipo de conexion es
+                if (this.Seguridad)  //Autenticacion de windows
+                {
+                    //Autenticacion de windows
+                    Cadena.ConnectionString = Cadena.ConnectionString + " Integrated Security = SSPI";
+                }
+                else
+                {
+                    //Autenticacion de SQL Server
+                    Cadena.ConnectionString = Cadena.ConnectionString + "User Id= " + this.Usuario + "; Password=" + this.Clave;
+                }
+            }
+            //Si tenemos un error
+            catch (Exception ex)
+            {
+                Cadena = null;
+                //Lanzo el error
+                throw ex;
+            }
+            return Cadena;
+        }
+
+        //Metodo para crear instancias de Conexion
+        public static Conexion getInstancia()
+        {
+            //Verificamos si tenemos instancia
+            if (Con == null)
+            {
+                Con = new Conexion();
+            }
+            return Con;
+        }
+    }
+}
